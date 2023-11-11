5e3a793e7afe23c07782bdde62f52c60d3175704
kartashows <144016301+kartashows@users.noreply.github.com>
2023-11-11 18:29:03 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,2 +1,5 @@
-CREATE ROLE crm WITH LOGIN PASSWORD 'crmcrm';
-CREATE DATABASE shevark OWNER crm;
+DO $$ BEGIN
+IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'shevark') THEN
+    CREATE DATABASE shevark OWNER crm;
+END IF;
+END $$;
